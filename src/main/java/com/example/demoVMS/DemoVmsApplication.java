ad3a9bb83ec98d8992f11e4edc1b58581497f40a
Þorvarður Bergmann Kjartansson <thoddi@pop-os.localdomain>
2020-08-19 17:23:12 +0000

    Initial commit

---
+++ src/main/java/com/example/demoVMS/DemoVmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoVMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoVmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoVmsApplication.class, args);
+	}
+
+}
